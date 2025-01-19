564017bc906f0e521288975275097eb4997ffe8f
TMDStudios <tmdstudioscontact@gmail.com>
2025-01-19 16:03:17 -0500

    Updated the look of projects and added better descriptions

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var apps = [
     {
         "name":"Block Wars",
-        "description": "<h4>Block Wars</h4><p>Shoot your way through an onslaught of blocks and enemy ships</p><p><ul><li>Blocks that get past you will stack, making the play area smaller</li><li>Use power-ups to destroy stacked blocks</li><li>Enemy ships get more difficult as the game progresses</li><li>Follow enemy patterns to anticipate their attacks</li></ul></p><p>How far can you get?</p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.blockwars"><img src="media/googleBanner.png"/></a>',
+        "description": `
+            <h4>Block Wars</h4>
+            <p>A fast-paced vertical shooter with a unique twist</p>
+            <ul>
+            <li>Falling blocks change color with each hit</li>
+            <li>Blocks are destroyed if they match the bullet color</li>
+            <li>Blocks stack to narrow the play area</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Java, Android Studio, LibGDX, Google SDK (Analytics, Google Pay, Leaderboards)</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play, Apple App Store, Amazon Appstore, itch.io</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.blockwars" target="_blank"><img src="media/googleBanner.png"/></a>',
         "video":"media/BlockWars.mp4",
         "poster":"media/BlockWars.png",
         "hasVideo":"true",
@@ -12,8 +25,21 @@ var apps = [
     },
     {
         "name":"Buddy Breakout",
-        "description": "<h4>Buddy Breakout</h4><p><ul><li>Free your buddies by breaking bricks that surround their cage</li><li>Defeat monsters to reach your buddies</li><li>Keep your ball alive by moving all 4 paddles</li><li>Collect cards to activate power-ups</li><li>Earn stars to unlock levels</li><li>Chase the high score in Survival Mode and Hardcore Mode</li></ul></p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.buddybreakout"><img src="media/googleBanner.png"/></a>',
+        "description": `
+            <h4>Buddy Breakout</h4>
+            <p>A new take on the classic breakout game</p>
+            <ul>
+            <li>Control four paddles to keep the ball in play</li>
+            <li>Bumper blocks add an element of surprise</li>
+            <li>Collect power-up cards to stay ahead of the game</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Java, Android Studio, LibGDX, Google SDK (Analytics, Google Pay, Leaderboards)</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play, Apple App Store, Amazon Appstore</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.buddybreakout" target="_blank"><img src="media/googleBanner.png"/></a>',
         "video":"media/BuddyBreakout.mp4",
         "poster":"media/BuddyBreakout.png",
         "hasVideo":"true",
@@ -23,8 +49,21 @@ var apps = [
     },
     {
         "name":"Puzzled Knight",
-        "description": "<h4>Puzzled Knight</h4><p><ul><li>Match tiles to attack your opponents or defend against their attacks</li><li>Earn gold to buy special moves to help you defeat your enemies</li><li>3 game modes, Arcade, Classic, and Tournament</li><li>Each opponent has their own strengths and weaknesses</li><li>Earn experience points to level up your character</li><li>Increase your stats to even the odds!</li></ul></p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.puzzledknight"><img src="media/googleBanner.png"/></a>',
+        "description": `
+            <h4>Puzzled Knight</h4>
+            <p>A match-3 adventure with strategic gameplay</p>
+            <ul>
+            <li>Attack, block, and evade against enemies that do the same</li>
+            <li>Gain extra turns by matching more than 3 blocks</li>
+            <li>Earn coins to purchase power-ups</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Java, Android Studio, LibGDX, Google SDK (Analytics, Google Pay, Leaderboards)</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play, Apple App Store, Amazon Appstore, itch.io, Kongregate</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.puzzledknight" target="_blank"><img src="media/googleBanner.png"/></a>',
         "video":"media/PuzzledKnight.mp4",
         "poster":"media/PuzzledKnight.png",
         "hasVideo":"true",
@@ -34,8 +73,21 @@ var apps = [
     },
     {
         "name":"Road To Rage",
-        "description": "<h4>Road To Rage</h4><p>Soothe your road rage by unloading a hood-mounted gun on your erratic fellow drivers. Can you get to your destination with your sanity intact?</p><p><ul><li>10 levels of rage relief</li><li>Increasing difficulty that will challenge your reflexes</li><li>A collection of 58 vehicles</li><li>Simple gameplay that is difficult to master</li></ul></p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.wordpress.tmdstudios"><img src="media/googleBanner.png"/></a>',
+        "description": `
+            <h4>Road To Rage</h4>
+            <p>A vertical shooter with 10 action-packed levels</p>
+            <ul>
+            <li>Blast your way through relentless traffic and chaos</li>
+            <li>Collect power-ups to unleash rampage mode and supercharge your weapons</li>
+            <li>Battle bosses at the end of each level</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Java, Android Studio, LibGDX, Google SDK (Analytics, Google Pay, Leaderboards)</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play, Apple App Store, Amazon Appstore, itch.io, Kongregate</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.wordpress.tmdstudios" target="_blank"><img src="media/googleBanner.png"/></a>',
         "video":"media/RTR.mp4",
         "poster":"media/RTR.png",
         "hasVideo":"true",
@@ -45,8 +97,21 @@ var apps = [
     },
     {
         "name":"Py Learning Companion",
-        "description": "<h4>Py Learning Companion</h4><p><ul><li>Light weight app with bite sized Python lessons and activities</li><li>Great for learning on the go</li><li>Multiple types of activities that will test your Python knowledge</li><li>Hundreds of activities to test your skills</li><li>Simple to use</li></ul></p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.python"><img src="media/googleBanner.png"/></a>',
+        "description": `
+            <h4>Py Learning Companion</h4>
+            <p>Light-weight app with bite-sized Python lessons and activities</p>
+            <ul>
+            <li>Great for learning on the go</li>
+            <li>Multiple types of activities that will test your Python knowledge</li>
+            <li>Hundreds of engaging activities to challenge your Python knowledge</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Java, Android Studio, LibGDX, Google SDK (Analytics, Google Pay)</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play, Apple App Store, Amazon Appstore</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.python" target="_blank"><img src="media/googleBanner.png"/></a>',
         "video":"media/PLC.mp4",
         "poster":"media/PLC.png",
         "hasVideo":"true",
@@ -55,30 +120,13 @@ var apps = [
         "gitHubLink":""
     },
     {
-        "name":"Crypto Ledger",
-        "description": "<h4>Crypto Ledger</h4><p>Crypto Ledger is an open source extension of the Crypto Ledger website. It makes use of the Crypto Ledger API to buy, sell, and monitor coins. The app was built using Kotlin and Android Studio. It uses coroutines and retrofit to make API calls, and fragments for navigation. The source code is available on <a style='text-decoration: none; color: #dedcde;' href='https://github.com/TMDStudios/crypto_ledger_app_kotlin'>GitHub</a></p><p>Buying and selling coins requires an API Key, while getting live price data does not.</p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.cryptoledgerkotlin"><img src="media/googleBanner.png"/></a>',
-        "video":"media/CryptoLedger.mp4",
-        "poster":"media/CryptoLedger.png",
-        "hasVideo":"true",
-        "thumb":"",
-        "fullImg":"",
-        "gitHubLink":""
-    },
-    {
-        "name":"Mock Trader",
-        "description": "<h4>Mock Trader</h4><p>The player starts with $10,000 and is tasked with accumulating as much wealth as possible by buying and selling Bitcoin</p><p>The Bitcoin price starts at $50,000 and changes based on randomized news</p><p>Can you become a (mock) billionaire?</p>",
-        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.mocktrader"><img src="media/googleBanner.png"/></a>',
-        "video":"media/MockTrader.mp4",
-        "poster":"media/MockTrader.png",
-        "hasVideo":"true",
-        "thumb":"",
-        "fullImg":"",
-        "gitHubLink":""
-    },
-    {
         "name":"MingSec",
-        "description": '<h4><a href="https://github.com/TMDStudios/MingSec">MingSec</a></h4><p>DIY home security system designed to leverage OpenCV for motion detection.</p>',
+        "description": `
+            <h5><a href="https://github.com/TMDStudios/MingSec" target="_blank">MingSec</a></h5>
+            <p>DIY home security system designed to leverage OpenCV for motion detection</p>
+            <hr>
+            <p>Python, OpenCV, Dropbox API, Django, REST API, JavaScript, Kotlin, Linux, SSH</p>
+        `,
         "link":"",
         "video":"",
         "poster":"",
@@ -89,7 +137,12 @@ var apps = [
     },
     {
         "name":"Game Room",
-        "description": '<h4><a href="https://gameroom.up.railway.app/">Game Room</a></h4><p>A platform for educators to play games and/or review materials with their students</p>',
+        "description": `
+            <h5><a href="https://gameroom.up.railway.app/" target="_blank">Game Room</a></h5>
+            <p>A platform for educators to play games and/or review materials with their students</p>
+            <hr>
+            <p>Java, Spring Boot, MySQL, JavaScript, WebSocket, SockJS, STOMP, jQuery, Docker</p>
+        `,
         "link":"",
         "video":"",
         "poster":"",
@@ -100,7 +153,12 @@ var apps = [
     },
     {
         "name":"Study Room",
-        "description": '<h4><a href="https://studyroom.tmdstudios.net/">Study Room</a></h4><p>Open-source platform for students to improve their English vocabulary and grammar</p>',
+        "description": `
+            <h5><a href="https://studyroom.tmdstudios.net/" target="_blank">Study Room</a></h5>
+            <p>Open-source platform for students to improve their English vocabulary and grammar</p>
+            <hr>
+            <p>JavaScript, REST API, HTML, CSS, AJAX, DOM Manipulation, Event Handling, XMLHttpRequest</p>
+        `,
         "link":"",
         "video":"",
         "poster":"",
@@ -111,7 +169,12 @@ var apps = [
     },
     {
         "name":"Ninja Stars",
-        "description": '<h4><a href="https://github.com/TMDStudios/ninja-stars">Ninja Stars</a></h4><p>A tool for students to organize around a topic.</p>',
+        "description": `
+            <h5><a href="https://github.com/TMDStudios/ninja-stars" target="_blank">Ninja Stars</a></h5>
+            <p>Tool designed to foster collaboration and support within bootcamp learning environments</p>
+            <hr>
+            <p>Python, Django, REST API, JWT, JavaScript, SPA, Chrome Extension, React</p>
+        `,
         "link":"",
         "video":"",
         "poster":"",
@@ -119,6 +182,54 @@ var apps = [
         "thumb":"media/NinjaStarsWebsiteThumb.png",
         "fullImg":"media/NinjaStarsWebsite.png",
         "gitHubLink":"https://github.com/TMDStudios/ninja-stars"
+    },
+    {
+        "name":"Crypto Ledger",
+        "description": `
+            <h4>Crypto Ledger</h4>
+            <p>Open source extension of the Crypto Ledger website</p>
+            <ul>
+            <li>Makes use of the Crypto Ledger API to buy, sell, and monitor coins</li>
+            <li>Buying and selling coins requires an API Key, while getting live price data does not</li>
+            <li>Currently unavailable due to pending deployment of the Crypto Ledger website</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Kotlin, Android Studio, REST API</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.cryptoledgerkotlin" target="_blank"><img src="media/googleBanner.png"/></a>',
+        "video":"media/CryptoLedger.mp4",
+        "poster":"media/CryptoLedger.png",
+        "hasVideo":"true",
+        "thumb":"",
+        "fullImg":"",
+        "gitHubLink":""
+    },
+    {
+        "name":"Mock Trader",
+        "description": `
+            <h4>Mock Trader</h4>
+            <p>Simple Bitcoin trading app</p>
+            <ul>
+            <li>Players make trades based on fictional daily news events</li>
+            <li>The app fetches JSON data to generate daily news stories</li>
+            <li>Deployed as a proof of concept for trading simulation</li>
+            </ul>
+            <h5>Tools Used:</h5>
+            <p>Kotlin, Android Studio, JSON</p>
+            <br>
+            <h5>Deployment:</h5>
+            <p>Google Play</p>
+        `,
+        "link":'<a href="https://play.google.com/store/apps/details?id=com.tmdstudios.mocktrader" target="_blank"><img src="media/googleBanner.png"/></a>',
+        "video":"media/MockTrader.mp4",
+        "poster":"media/MockTrader.png",
+        "hasVideo":"true",
+        "thumb":"",
+        "fullImg":"",
+        "gitHubLink":""
     }
 ];
 var currentApp = 0;
@@ -142,7 +253,13 @@ function nextApp(){
 }
 
 function showApp(){
+    const isSmallScreen = window.matchMedia("(max-width: 975px)").matches;
     if(apps[currentApp].hasVideo=="true"){
+        if(!isSmallScreen){
+            document.getElementById("project-right").style.display = "block";
+        }else{
+            document.getElementById("project-right").style.display = "none";
+        }
         document.getElementById("mobileLink").innerHTML = '<p class="storeLink" id="link"><a href="https://play.google.com/store/apps/details?id=com.tmdstudios.blockwars"><img src="media/googleBanner.png"/></a></p>';
         document.getElementById("mediaContainer").innerHTML = '<video id="video" width="320" height="480" autoplay muted controls loop poster="media/BlockWars.png"><source src="media/BlockWars.mp4" type="video/mp4">Your browser does not support the video tag.</video>'
         document.getElementById("description").innerHTML = apps[currentApp].description;
@@ -151,7 +268,8 @@ function showApp(){
         document.getElementById("video").setAttribute("poster", apps[currentApp].poster);
     }else{
         document.getElementById("mediaContainer").innerHTML = '<img id="websiteImg" onclick="viewImage()" src="'+apps[currentApp].thumb+'"/><div class="websiteInfo">'+apps[currentApp].description+'</div>';
-        document.getElementById("description").innerHTML = '<p style="text-align: center;"><img class="gitHubImg" src="media/GitHub.png"/></p><h6><a href='+apps[currentApp].gitHubLink+'>'+apps[currentApp].name+'<br>Source Code</a></h6>';
+        document.getElementById("description").innerHTML = '';
+        document.getElementById("project-right").style.display = "none";
         document.getElementById("link").innerHTML = "";
         document.getElementById("mobileLink").innerHTML = "";
     }
